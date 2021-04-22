b23a810c1d8ba0733de52634af5ccbee2ad156b7
Bryan Nguyen <bryanthenguyener@gmail.com>
2021-04-22 00:09:59 -0500

    some basic styling and upload file

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="/styles.css" rel="stylesheet">
 	<title>Cashapp Transaction Tracker</title>
 </head>
-<body>
-    <h1>Hello!</h1>
+<body class = "body">
+    <div class = "header">
+        <h1>Cashapp Transaction Tracker</h1>
+    </div>
 
+    <div class = "upload-file">
+        Please upload your Cashapp CSV file! <br>
+        <input type="file" name="csv_file">
+    </div>
+
+    
 </body>
 </html>
