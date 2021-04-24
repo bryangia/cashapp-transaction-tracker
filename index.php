8acd793d1df0fb8615fabcf7280bb78ae3532c55
Bryan Nguyen <bryanthenguyener@gmail.com>
2021-04-23 20:38:29 -0500

    dropin file

--- index.php
+++ index.php
@@ -8,12 +8,21 @@
     <div class = "header">
         <h1>Cashapp Transaction Tracker</h1>
     </div>
-
-    <div class = "upload-file">
-        Please upload your Cashapp CSV file! <br>
-        <input type="file" name="csv_file">
+    <div style= "text-align: center">
+        <form action="" method="post" enctype="multipart/form-data">
+        <p class = "upload-file"> Please upload your Cashapp CSV file! </p><br>
+            <div class="drop-zone">
+                <span class="drop-zone__prompt">Drop Cashapp csv file here or click to upload</span>
+                <input type="file" name="csv_file" id="csv_file" class="drop-zone__input">
+            </div> <br>
+            <input type = "hidden" name = "submit_file" id = "submit_file" value = "Submit File">
+            <button class = "submit-button"> Submit File </button>
+        </form>
     </div>
-
-    
+    <script src="/main.js"></script>
 </body>
+<?php
+
+?>
+
 </html>
