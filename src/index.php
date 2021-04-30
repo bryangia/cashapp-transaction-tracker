dcfdc105b66842aa0cc40d4ae0bb81605b687335
Bryan Nguyen <bryangia@BryanSurface.localdomain>
2021-04-30 12:51:09 -0500

    currently can submit but doesn't take correct date values into post

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php 
     function createDateMatrix($startDate, $endDate) {
-        $begin = new DateTime('2010-05-01');
-        $end = new DateTime('2010-05-10');
+        $begin = new DateTime($startDate);
+        $end = new DateTime($endDate);
         $interval = DateInterval::createFromDateString('1 day');
         $period = new DatePeriod($begin, $interval, $end);
 
@@ -28,23 +28,35 @@
     </div>
     <?php if(!isset($_POST['submit_file'])) { ?>
     <div style= "text-align: center">
+        <p class = "upload-file"> Please upload your cash_app_report.csv file! </p><br>
+        <label for="view_method"> Report Method: </label>
+        <select id = "view_method" name = "view_method" onchange='formSelect(this.value)'> 
+                <option value="last_week" selected> Last 7 Days </option>
+                <option value="time_frame"> Specific Timeframe</option>
+        </select> <br> <br>
+        
         <form action="" method="post" enctype="multipart/form-data">
-            <p class = "upload-file"> Please upload your cash_app_report.csv file! </p><br>
-            <label for="view_method"> Report Method: </label>
-            <select id="method" name = "view_method" onchange='formSelect(this.value)'> 
-                    <option value="last_week" selected> Last 7 Days </option>
-                    <option value="time_frame"> Specific Timeframe</option>
-            </select> <br> <br>
-            <div id="date_input" style="display:none">    
-                Start Date: <input type='date' id='start' name='sales_start' required/>
-                End Date: <input type='date' id='end' name='sales_end' required/>
+            <div action="" method="post" id = "week_dates">
+                <?php
+                $endDate = date("Y-m-d");
+                $startDate = date("Y-m-d", strtotime("-7 days"));
+                ?>
+                Start Date: <?php echo $startDate . " "; ?> End Date: <?php echo $endDate;?>
+                <input type="hidden" id = "last_week_start" name="start_date" value="<?php echo $startDate;?>">
+                <input type="hidden" id = "last_week_end" name="end_date" value="<?php echo $endDate;?>">
+            </div>
+            <div action="" method="post" id="date_input" style=display:none>
+                Start Date: <input type='date' id='date_input_start' name='start_date'/>
+                End Date: <input type='date' id='date_input_end' name= 'end_date'/>
+            </div>
+            <br>
+
+            <div class="drop-zone">
+                <span class="drop-zone__prompt">Drop Cashapp csv file here or click to upload</span>
+                <input type="file" name="csv_file" id="csv_file" class="drop-zone__input">
             </div> <br>
-                <div class="drop-zone">
-                    <span class="drop-zone__prompt">Drop Cashapp csv file here or click to upload</span>
-                    <input type="file" name="csv_file" id="csv_file" class="drop-zone__input">
-                </div> <br>
-                <input type = "hidden" name = "submit_file" id = "submit_file" value = "Submit File">
-                <button class = "submit-button"> Submit File </button>
+            <input type = "hidden" name = "submit_file" id = "submit_file" value = "Submit File">
+            <button type = "submit/button" class = "submit-button"> Submit File </button>
         </form>
     </div>
     <?php } ?>
@@ -54,17 +66,19 @@
         if(isset($_POST['submit_file'])) {
             
             if($_FILES['csv_file']['name'] != "" && str_ends_with($_FILES['csv_file']['name'], '.csv')) {
-                echo "FUNCTIONALITY UNDER CONSTRUCTION";
+                echo "FUNCTIONALITY UNDER CONSTRUCTION <br>";
                 echo "File name: ";
                 echo $_FILES['csv_file']['name'];
                 echo " " . $_FILES['csv_file']['tmp_name'] . "<br>";
-                $dateMatrix = createDateMatrix();
-                
+
+                echo "Start Date: " . $_POST['start_date'] . "<br> End Date: " . $_POST['end_date'] . "<br>";
+                #$dateMatrix = createDateMatrix();
+                echo $_SESSION['start_date'] . "<br>";
                 $fileTmpPath = $_FILES['csv_file']['tmp_name'];
                 $fileName = $_FILES['csv_file']['name'];
                 $file = fopen($_FILES['csv_file']['tmp_name'], 'r');
                 while($transaction = fgetcsv($file)) {
-                    echo $transaction[1];
+                    echo $transaction[1] . "<br>";
                 }
 
             }
@@ -80,14 +94,17 @@
         if(isset($_POST['submit_new_file'])) {
             header("Location:/src/index.php");
         }
+        #document.getElementById("myText").disabled = true;
     ?>
 </body>
     <script>
     function formSelect(_option) {
         if (_option == 'time_frame') {
             document.getElementById('date_input').style.display = "block";
+            document.getElementById('week_dates').style.display = "none";
         }
-        else {
+        else if(_option == 'last_week'){
+            document.getElementById('week_dates').style.display = "block";
             document.getElementById('date_input').style.display = "none";
         }
     }
