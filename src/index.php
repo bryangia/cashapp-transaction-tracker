2e17ffcbffc71ac90e81c862c48acdafce84aa9a
Bryan Nguyen <bryangia@BryanSurface.localdomain>
2021-04-30 10:38:09 -0500

    form for date complete

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php 
-    function createDateMatrix() {
+    function createDateMatrix($startDate, $endDate) {
         $begin = new DateTime('2010-05-01');
         $end = new DateTime('2010-05-10');
         $interval = DateInterval::createFromDateString('1 day');
@@ -29,19 +29,22 @@
     <?php if(!isset($_POST['submit_file'])) { ?>
     <div style= "text-align: center">
         <form action="" method="post" enctype="multipart/form-data">
-        <p class = "upload-file"> Please upload your cash_app_report.csv file! </p><br>
-        <label for="view_method"> Report Method: </label>
-        <select id="method" name = "view_method">
-                <option value="last_week" selected> Last 7 Days </option>
-                <option value="time_frame"> Specific Timeframe</option>
-        </select> <br> <br> Start Date: <input type='date' id='start' name='sales_start' required/>
-                    End Date: <input type='date' id='end' name='sales_end' required/>
-            <div class="drop-zone">
-                <span class="drop-zone__prompt">Drop Cashapp csv file here or click to upload</span>
-                <input type="file" name="csv_file" id="csv_file" class="drop-zone__input">
+            <p class = "upload-file"> Please upload your cash_app_report.csv file! </p><br>
+            <label for="view_method"> Report Method: </label>
+            <select id="method" name = "view_method" onchange='formSelect(this.value)'> 
+                    <option value="last_week" selected> Last 7 Days </option>
+                    <option value="time_frame"> Specific Timeframe</option>
+            </select> <br> <br>
+            <div id="date_input" style="display:none">    
+                Start Date: <input type='date' id='start' name='sales_start' required/>
+                End Date: <input type='date' id='end' name='sales_end' required/>
             </div> <br>
-            <input type = "hidden" name = "submit_file" id = "submit_file" value = "Submit File">
-            <button class = "submit-button"> Submit File </button>
+                <div class="drop-zone">
+                    <span class="drop-zone__prompt">Drop Cashapp csv file here or click to upload</span>
+                    <input type="file" name="csv_file" id="csv_file" class="drop-zone__input">
+                </div> <br>
+                <input type = "hidden" name = "submit_file" id = "submit_file" value = "Submit File">
+                <button class = "submit-button"> Submit File </button>
         </form>
     </div>
     <?php } ?>
@@ -79,5 +82,14 @@
         }
     ?>
 </body>
-
+    <script>
+    function formSelect(_option) {
+        if (_option == 'time_frame') {
+            document.getElementById('date_input').style.display = "block";
+        }
+        else {
+            document.getElementById('date_input').style.display = "none";
+        }
+    }
+    </script>
 </html>
